b75754feb95d0b6a218d6009e5536eb3bdd486bb
The Judge <53906078+thejudge156@users.noreply.github.com>
2023-10-29 11:30:40 -0700

    Update BalanceChassisConstants.java

--- src/main/java/frc8768/robot/auto/BalanceChassisConstants.java
+++ src/main/java/frc8768/robot/auto/BalanceChassisConstants.java
@@ -3,7 +3,7 @@ package frc8768.robot.auto;
 public class BalanceChassisConstants {
 	public static final double kGroundAngle = 0.0;
 	public static final double kTolerance = 4;
-	public static final double kDriveSpeedMPS = 0.6;
+	public static final double kDriveSpeedMPS = 0.2;
 	public static final double kP = 0.045849;
 	public static final double kI = 0;
 	public static final double kD = 0.0068069;

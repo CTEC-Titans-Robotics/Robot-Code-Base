665d6b013f9c638a82a4ed5d13b626e237f871c8
TheJudge156 <colepatrickroberts@gmail.com>
2024-02-15 17:27:22 -0800

    Fix syntax

--- src/main/java/frc8768/robot/auto/Auto.java
+++ src/main/java/frc8768/robot/auto/Auto.java
@@ -45,6 +45,7 @@ public class Auto {
                 swerveDrive::getRobotVelocity,
                 swerveDrive::setChassisSpeeds,
                 config,
+                () -> false,
                 swerve);
 
         autonChooser = new SendableChooser<>();

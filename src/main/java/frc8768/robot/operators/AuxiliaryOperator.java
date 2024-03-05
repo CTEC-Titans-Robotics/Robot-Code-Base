97df8a858729fe8456dd444cdd697d09750190d2
ctecrobotics <ctectitansrobotics@gmail.com>
2024-03-04 17:42:40 -0800

    Y is left and right???

--- src/main/java/frc8768/robot/operators/AuxiliaryOperator.java
+++ src/main/java/frc8768/robot/operators/AuxiliaryOperator.java
@@ -51,7 +51,7 @@ public class AuxiliaryOperator extends Operator {
         } else if(controller.getLeftBumper()) {
             if(distance != -1 && Constants.SPEAKER_IDS.contains(this.vision.getTargetID())) {
                 if(MathUtil.isNear(0,
-                        this.vision.getBestTarget().getBestCameraToTarget().getX(), 0.0508)) {
+                        this.vision.getBestTarget().getBestCameraToTarget().getY(), 0.0508)) {
                     this.caNdle.setLEDs(0, 255, 0);
                 }
                 this.arm.overrideAngle = MathUtil.clamp(Math.pow(distance, 0.731) + 21.5, 2, 85);

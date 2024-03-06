50c5ed40807cb78dcb3aaba923586f2e76f66f8f
The Judge <colepatirckroberts@gmail.com>
2024-03-05 22:14:39 -0800

    Add commands

--- src/main/java/frc8768/robot/auto/Auto.java
+++ src/main/java/frc8768/robot/auto/Auto.java
@@ -4,6 +4,7 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
 import com.pathplanner.lib.util.PIDConstants;
 import com.pathplanner.lib.util.ReplanningConfig;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -43,11 +44,22 @@ public class Auto {
                 swerveDrive::getRobotVelocity,
                 swerveDrive::setChassisSpeeds,
                 config,
-                () -> false, // Change if needed
+                () -> DriverStation.getAlliance().get() == DriverStation.Alliance.Red ? true : false, // Change if needed
                 swerve);
 
         this.autonChooser = new SendableChooser<>();
         this.autonChooser.setDefaultOption("No-op", new InstantCommand());
+
+        // Setup Positions
+        this.autonChooser.addOption("Position 1", AutoBuilder.buildAuto("Position 1"));
+        this.autonChooser.addOption("Position 2", AutoBuilder.buildAuto("Position 2"));
+        this.autonChooser.addOption("Position 3 N1", AutoBuilder.buildAuto("Position 3 N1"));
+        this.autonChooser.addOption("Position 3 N2", AutoBuilder.buildAuto("Position 3 N2"));
+        this.autonChooser.addOption("Position 3 N3", AutoBuilder.buildAuto("Position 3 N3"));
+        this.autonChooser.addOption("Position 4 N2", AutoBuilder.buildAuto("Position 4 N2"));
+        this.autonChooser.addOption("Position 4 N3", AutoBuilder.buildAuto("Position 4 N3"));
+        this.autonChooser.addOption("Position 5", AutoBuilder.buildAuto("Position 5"));
+        this.autonChooser.addOption("Position 6", AutoBuilder.buildAuto("Position 6"));
     }
 
     /**

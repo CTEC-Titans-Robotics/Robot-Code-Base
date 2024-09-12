549f2ae38cc61375acfe3c9fd270a927cd016159
ctecrobotics <ctectitansrobotics@gmail.com>
2024-09-11 18:03:38 -0700

    Moved auto translation code into function

--- src/main/java/frc8768/robot/auto/Auto.java
+++ src/main/java/frc8768/robot/auto/Auto.java
@@ -1,10 +1,14 @@
 package frc8768.robot.auto;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.config.ModuleConfig;
-import com.pathplanner.lib.config.PIDConstants;
-import com.pathplanner.lib.config.RobotConfig;
-import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -62,11 +66,45 @@ public class Auto {
         autonChooser.setDefaultOption("No-op", new InstantCommand());
     }
 
+
+
     /**
      * Get the current Auton mode
      *
      * @return Current Auton Mode
      */
+    public void Translation(double x , double y ) {
+        /*
+        if(auto != null) {
+            if(!auto.getSelected().isScheduled()) {
+                return;
+            }
+            auto.getSelected().execute();
+        }
+        */
+
+            // Get the current pose from the odometry
+        Pose2d currentPose = swerve.getSwerveDrive().getPose();
+        double currentX = currentPose.getX();
+        double currentY = currentPose.getY();
+
+        // Drive forward if the current distance is less than the target distance
+        if (currentX < x) {
+            swerve.getSwerveDrive().drive(
+                    new ChassisSpeeds(1.0, 0.0, 0.0)); // 1.0 m/s forward, no strafing, no rotation
+        }
+
+        if (currentY < y) {
+            swerve.getSwerveDrive().drive(
+                    new ChassisSpeeds(0.0, 1.0, 0.0)); // 1.0 m/s forward, no strafing, no rotation
+        } else {
+            // Stop the robot once the target distance is reached
+            swerve.getSwerveDrive().drive(
+                    new ChassisSpeeds(0.0, 0.0, 0.0));
+        }
+    }
+
+
     public Command getSelected() {
         return autonChooser.getSelected();
     }
